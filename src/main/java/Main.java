5fc41bdf1da03ee208790e54855de7729c245930
Roman Bulinski <aboandrus@gmail.com>
2019-07-05 21:11:16 +0200

    Refactor code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+
 
 public class Main {
 
@@ -8,20 +6,15 @@ public class Main {
     public static int[] sumParts(int[] ls) {
 
         int[] result = new int[ls.length + 1];
-        int[] tempArr = new int[ls.length ];
 
-        int position = 0;
-        for(int i = ls.length-1; i>=0; i--){
-            tempArr[position] = ls[i];
-            position++;
-        }
-        int posytion2 = 0;
+        int posytion = ls.length-1;
         int sum=0;
         for( int i = result.length-1; i>=0; i--){
             result[i]=sum;
-            if(posytion2<tempArr.length){
-                sum =sum + tempArr[posytion2];
-                posytion2++;
+
+            if(posytion>=0){
+                sum =sum + ls[posytion];
+                posytion--;
             }
         }
         return result;
@@ -29,17 +22,6 @@ public class Main {
 
 
 
-//    public static void main(String[] args) {
-//
-//        int[] ls = {1,2,3};
-//
-//        int[] tttt = sumParts(ls);
-//
-//        for(int n : tttt){
-//            System.out.println(n);
-//        }
-//
-//
-//    }
+
 
 }
